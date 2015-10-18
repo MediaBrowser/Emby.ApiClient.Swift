011a397391ec34ff458b231b824122ac28bf1992
Corey Vaillancourt <corey@coreys-macbook-pro.local>
2015-10-17 22:48:34 -0400

    Supporting files for ServerDiscoveryProtocol

---
+++ Emby.ApiClient/model/apiclient/ServerDiscoveryInfo.swift
@@ -0,0 +1,12 @@
+//
+//  ServerDiscoveryInfo.swift
+//  EmbyApiClient
+//
+
+import Foundation
+
+public struct ServerDiscoveryInfo {
+    let address: String
+    let id: String
+    let name: String
+}
