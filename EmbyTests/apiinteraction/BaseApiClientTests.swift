931dcf63b46b2f09442e79e0c1552588f7dd142b
Corey Vaillancourt <corey@coreys-macbook-pro.local>
2015-11-21 23:51:11 -0500

    - Configured for unit tests - Added two unit tests for BaseApiClient - Set Emby ApiClient scheme as shared.

---
+++ EmbyTests/apiinteraction/BaseApiClientTests.swift
@@ -0,0 +1,43 @@
+//
+//  BaseApiClientTests.swift
+//  Emby.ApiClient
+//
+
+import XCTest
+@testable import Emby_ApiClient
+
+class BaseApiClientTests: XCTestCase {
+    let logger = Logger()
+    let jsonSerializer = JsonSerializer()
+    let serverAddress = "http://localhost"
+    let clientName = "TestClient"
+    let device = iOSDevice()
+    let applicationVersion = "1"
+    var baseApiClient: BaseApiClient?
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        self.baseApiClient = BaseApiClient(logger: logger, jsonSerializer: jsonSerializer, serverAddress: serverAddress, clientName: clientName, device: device, applicationVersion: applicationVersion)
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func testApiUrl() {
+        let apiUrl = baseApiClient!.getApiUrl()
+        
+        XCTAssertEqual("\(serverAddress)/mediabrowser", apiUrl)
+    }
+    
+    func testSubtitleUrl() {
+        let subtitleDownloadOptions = SubtitleDownloadOptions(itemId: "1", mediaSourceId: "2", streamIndex: 3, format: "x")
+        
+        let subtitleUrl = baseApiClient!.getSubtitleUrl(subtitleDownloadOptions)
+        
+        XCTAssertEqual("\(serverAddress)/mediabrowser/Videos/1/2/Subtitles/3/Stream.x?", subtitleUrl)
+    }
+}
