490b2ffa41aa64d9976f66f34243d28c501bf9e8
Corey Vaillancourt <corey@coreys-macbook-pro.local>
2015-12-30 22:56:12 -0500

    Partial SessionInfoDto implementation

--- Emby.ApiClient/model/session/PlayerStateInfo.swift
+++ Emby.ApiClient/model/session/PlayerStateInfo.swift
@@ -5,15 +5,43 @@
 
 import Foundation
 
-public struct PlayerStateInfo {
-    let positionTicks: Int
+public struct PlayerStateInfo : JSONSerializable {
+    let positionTicks: Int?
     let canSeek: Bool
     let isPaused: Bool
     let isMuted: Bool
-    let volumeLevel: Int
-    let audioStreamIndex: Int
-    let subtitleStreamIndex: Int
-    let mediaSourceId: String
-    let playMethod: PlayMethod
-    let repeatMode = RepeatMode.RepeatNone
+    let volumeLevel: Int?
+    let audioStreamIndex: Int?
+    let subtitleStreamIndex: Int?
+    let mediaSourceId: String?
+    let playMethod: PlayMethod?
+    var repeatMode = RepeatMode.RepeatNone
+    
+    public init?(jSON: JSON_Object) {
+        if  let canSeek = jSON["CanSeek"] as? Int,
+            let isPaused = jSON["IsPaused"] as? Int,
+            let isMuted = jSON["IsMuted"] as? Int,
+            let repeatMode = RepeatMode(rawValue: (jSON["RepeatMode"] as? String)!)
+        {
+            self.positionTicks = jSON["PositionTicks"] as? Int
+            self.canSeek = Bool(canSeek)
+            self.isPaused = Bool(isPaused)
+            self.isMuted = Bool(isMuted)
+            self.volumeLevel = jSON["VolumeLevel"] as? Int
+            self.audioStreamIndex = jSON["AudioStreamIndex"] as? Int
+            self.mediaSourceId = jSON["MediaSourceId"] as? String
+            
+            if let playMethod = jSON["PlayMethod"] as? Int {
+                self.playMethod = PlayMethod(rawValue: playMethod)
+            } else {
+                self.playMethod = nil
+            }
+            
+            self.repeatMode = repeatMode
+            self.subtitleStreamIndex = jSON["SubtitleStreamIndex"] as? Int
+        }
+        else {
+            return nil
+        }
+    }
 }
--- Emby.ApiClient/model/session/SessionInfoDto.swift
+++ Emby.ApiClient/model/session/SessionInfoDto.swift
@@ -6,28 +6,69 @@
 import Foundation
 
 public struct SessionInfoDto : JSONSerializable {
-//    var supportedCommands = [String]()
-//    var queueableMediaTypes = [String]()
-//    var playableMediaTypes = [String]()
-//    let id: String
-//    let userId: String
-//    let userPrimaryImageTag: String
-//    let userName: String
+      var supportedCommands = [String]()
+      var queueableMediaTypes = [String]()
+      var playableMediaTypes = [String]()
+      let id: String
+      let userId: String
+      let userPrimaryImageTag: String?
+      let userName: String
 //    var additionalUsers = [SessionUserInfo]()
-//    let applicationVersion: String
-//    let client: String
-//    let lastActivityDate = NSDate()
+      let applicationVersion: String
+      let client: String
+      var lastActivityDate = NSDate()
 //    let nowViewingItem: BaseItemInfo
-//    let deviceName: String
+      let deviceName: String
 //    let nowPlayingItem: BaseItemInfo
-//    let deviceId: String
-//    let appIconUrl: String
-//    let supportsRemoteControl: Bool
-//    let playState: PlayerStateInfo
-//    let playstateInfo: PlayerStateInfo
+      let deviceId: String
+      let appIconUrl: String?
+      let supportsRemoteControl: Bool
+      let playState: PlayerStateInfo
 //    let transcodingInfo: TranscodingInfo
     
     public init?(jSON: JSON_Object) {
-        fatalError("init(jSON:) has not been implemented: \(jSON)")
+        if  let queueableMediaTypes = jSON["QueueableMediaTypes"] as? [String],
+            let playableMediaTypes = jSON["PlayableMediaTypes"] as? [String],
+            let deviceId = jSON["DeviceId"] as? String,
+            let supportsRemoteControl = jSON["SupportsRemoteControl"] as? Int,
+            let userId = jSON["UserId"] as? String,
+            let deviceName = jSON["DeviceName"] as? String,
+            let id = jSON["Id"] as? String,
+            let userName = jSON["UserName"] as? String,
+            let playstateJSON = jSON["PlayState"] as? JSON_Object,
+            let playState = PlayerStateInfo(jSON: playstateJSON),
+            let supportedCommands = jSON["SupportedCommands"] as? [String],
+            let client = jSON["Client"] as? String,
+            let lastActivityDateString = jSON["LastActivityDate"] as? String,
+            let applicationVersion = jSON["ApplicationVersion"] as? String
+        {
+            self.queueableMediaTypes = queueableMediaTypes
+            self.playableMediaTypes = playableMediaTypes
+            self.deviceId = deviceId
+            self.appIconUrl = jSON["AppIconUrl"] as? String
+            self.supportsRemoteControl = Bool(supportsRemoteControl)
+            self.userId = userId
+            self.userPrimaryImageTag = jSON["UserPrimaryImageTag"] as? String
+            self.deviceName = deviceName
+            self.id = id
+            self.userName = userName
+            self.playState = playState
+            self.supportedCommands = supportedCommands
+            self.client = client
+            
+            let formatter = NSDateFormatter()
+            formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
+            formatter.timeZone = NSTimeZone(forSecondsFromGMT: 0)
+            
+            self.lastActivityDate = formatter.dateFromString(lastActivityDateString)!
+            self.applicationVersion = applicationVersion
+            
+            //MARK: TODO Additional Users
+            //MARK: TODO Now Viewing Item
+            //MARK: TODO Transcoding Info
+        }
+        else {
+            return nil
+        }
     }
 }
